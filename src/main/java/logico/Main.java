81e82074585f7bd9f97e53f3155bb5ea41f60ab1
marco <20160370@ce.pucmm.edu.do>
2019-11-25 21:18:35 -0400

    Extrayendo información del usuario, creación de links.

--- src/main/java/logico/Main.java
+++ src/main/java/logico/Main.java
@@ -1,12 +1,11 @@
 package logico;
 
-import services.BootStrapServices;
-import services.DataBaseServices;
-import services.GestionDB;
-import services.UsuarioServices;
+import org.apache.commons.codec.digest.DigestUtils;
+import services.*;
 
 import javax.naming.ldap.Control;
 import java.sql.SQLException;
+import java.util.UUID;
 
 public class Main {
 
@@ -18,28 +17,25 @@ public class Main {
         new Filtros().manejarFiltros();
         new GestionDB<>();
         UsuarioServices usuarioServices = new UsuarioServices();
+        VisitaServices visitaServices = new VisitaServices();
 
         Usuario usuario = null;
         if(usuarioServices.getSizeUsuario() == 0){
-            //usuario = new Usuario("Admin", "Admin", "admin", true);
+            usuario = new Usuario("Admin", "Admin", DigestUtils.md5Hex("admin"), true);
+            usuario.setId(UUID.randomUUID().toString());
+            Usuario usuarioAnonimo = new Usuario("Anonimo", "Anonimo", DigestUtils.md5Hex(usuario.getId()), false);
+            usuarioServices.crear(usuario);
+            usuarioServices.crear(usuarioAnonimo);
             System.out.println("Creando a un admin...");
         }
         else{
             System.out.println("El admin no ha sido creado");
         }
 
-        Url url = new Url("youtube.com");
-        String shortenedUrl = new Codec().encode(url.getUrlOriginal());
-        url.setUrlBase62(shortenedUrl);
-        url.setUrlIndexada(Integer.toString(Controladora.getInstance().getMisUrls().size()+1));
-        url.setCreador(usuario);
-        Controladora.getInstance().getMisUrls().add(url);
-
-
-        //Codec codec = new Codec();
-
-        //codec.encode("https://www.youtube.com/watch?v=5bBfEt5W1Nc");
-        //codec.encode("https://www.youtube.com/watch?v=5bBfEt5W1ac");
+        long visitsByChrome = visitaServices.getSizeVisitaByBrowser("Chrome");
+        long visitsByChromeAndPage = visitaServices.getSizeVisitaByShortUrlBrowser("/shorty.com/c", "Chrome");
+        System.out.println("La cantidad de visitas por chrome es: " + visitsByChrome);
+        System.out.println("La cantidad de visitas por chrome en la pag /shorty.com/c es: " + visitsByChromeAndPage);
     }
 
 }
