60d32675c27a1cf0fcf0fe92edd8fcf6499d0209
SaulFeliciano02 <bobxmugiwara@gmail.com>
2019-11-30 03:56:03 -0400

    heroku port

--- src/main/java/logico/Main.java
+++ src/main/java/logico/Main.java
@@ -12,9 +12,12 @@ import javax.naming.ldap.Control;
 import java.sql.SQLException;
 import java.util.UUID;
 
+import static spark.Spark.port;
+
 public class Main {
 
     public static void main(String[] args) throws SQLException {
+        port(getHerokuAssignedPort());
         Spark.staticFileLocation("/publico");
         BootStrapServices.startDb();
         DataBaseServices.getInstancia().testConexion();
@@ -44,4 +47,15 @@ public class Main {
         System.out.println("La cantidad de visitas por chrome en la pag /shorty.com/c es: " + visitsByChromeAndPage);
     }
 
+    /**
+     * Metodo para setear el puerto en Heroku
+     * @return
+     */
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //Retorna el puerto por defecto en caso de no estar en Heroku.
+    }
 }
